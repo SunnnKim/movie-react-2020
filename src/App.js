5565acb9787150f4df5d09d69859660106ed0d65
Sun ju Kim <ksunju0610@naver.com>
2020-09-07 08:34:30 +0900

    React : props

--- src/App.js
+++ src/App.js
@@ -1,54 +1,64 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
 function Food({data}){
   return <div>
-  <h1>
-      I like {data.name}
-      {/* <img src={data.image}/> */}
-  
-    </h1>
+  <div>
+      <h2>I like {data.name}</h2>
+      <img src={data.image} alt={data.name}></img>
+    </div>
 </div>
+}
+
+// rendering 함수 만들기
+function renderFood(dish){
+  console.log(dish)
+  return <Food key={dish.id} data={dish}></Food>
+
 }
 
 // 리스트를 동적으로 생성하기
 const foodILike= [
   {
+    id:1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      , rating : 5
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
+    , rating:4.3
+    },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    ,rating:3.3
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+    , rating : 4.7
   }
 ];
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      <Food data = 
-        {foodILike.map((current)=>{
-          console.log(current);
-          return current;
-      })} 
-      />
+      {foodILike.map(renderFood)} 
     </div>
   );
 }
